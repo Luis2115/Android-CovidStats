1df734e0e739c210c2c65c2320e6c17c73c2cf67
Luis2115 <lmorochofebres@gmail.com>
2021-05-23 23:50:04 -0500

    Agregando los Fragments con sus Clases, Layouts y un Toolbar Personalizado

---
+++ app/src/main/java/com/reymi/covid_stats_global/Fragments/CountryFragment.java
@@ -0,0 +1,33 @@
+package com.reymi.covid_stats_global.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.reymi.covid_stats_global.R;
+
+public class CountryFragment extends Fragment {
+
+    View view;
+    public CountryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_country, container, false);
+
+        return view;
+    }
+}
