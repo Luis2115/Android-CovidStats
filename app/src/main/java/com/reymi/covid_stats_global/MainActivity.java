9cd4322a9bfef2b71036db26474e8eae28164adc
Luis2115 <lmorochofebres@gmail.com>
2021-05-23 23:53:15 -0500

    Programando la Actividad Principal, y asignando el destino de los Fragments

--- app/src/main/java/com/reymi/covid_stats_global/MainActivity.java
+++ app/src/main/java/com/reymi/covid_stats_global/MainActivity.java
@@ -1,14 +1,58 @@
 package com.reymi.covid_stats_global;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.reymi.covid_stats_global.Fragments.CountryFragment;
+import com.reymi.covid_stats_global.Fragments.HomeFragment;
+import com.reymi.covid_stats_global.Fragments.InfoFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    HomeFragment homeFragment = new HomeFragment();
+    CountryFragment countryFragment = new CountryFragment();
+    InfoFragment infoFragment = new InfoFragment();
+    BottomNavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        setSupportActionBar(toolbar);
+
+        navigation = findViewById(R.id.btnNavigation);
+        navigation.setOnNavigationItemSelectedListener(interfaceBottonNavigation);
+
+        loadFragment(homeFragment);
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener interfaceBottonNavigation = item -> {
+        switch (item.getItemId()) {
+            case R.id.menuHome:
+                loadFragment(homeFragment);
+                return true;
+            case R.id.menuCountry:
+                loadFragment(countryFragment);
+                return true;
+            case R.id.menuAbout:
+                loadFragment(infoFragment);
+                return true;
+        }
+        return false;
+    };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.containerFragment, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 }
